0978f7495a942f7b009ecfcd8ac050cfe28c5771
Savannah <savmaxwell@gmail.com>
2021-10-13 11:34:10 -0600

    completed tasks 1-4

--- index.js
+++ index.js
@@ -39,13 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
 
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
 
-
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
 
 
 
@@ -63,9 +75,24 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
+
+Car.prototype.drive = function(distance){
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank - (distance / this.milesPerGallon);
+    if(this.tank === 0){
+      return `I ran out of fuel at ${odometer} miles!`;
+    }
+  }
 
 
 /*
@@ -75,18 +102,25 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
+ }
  
-}
+Baby.prototype = Object.create(Person.prototype);
+
+ Baby.prototype.play = function(){
+   return `Playing with ${this.favoriteToy}`;
+ }
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. New Binding - only used in constructor functions, 'this' is defined by the specific instance of the object that is created and returned by the constructor function
+  2. Implicit Binding - the most common binding rule, when a function is invoked 'this' is defined by whatever is to the left of the dot ( . )
+  3. Explicit Binding - applies when .call() or .apply() is used, 'this' is explicitly defined by passing the object into .call() or .apply() as an argument
+  4. Window Binding - if none of the above apply, 'this' will default to the window meaning the browser will try to define 'this' using everything it deems accurate
 */
 
 
